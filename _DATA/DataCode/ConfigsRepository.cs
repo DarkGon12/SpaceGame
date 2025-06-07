0ac89039800982f47a0797e4d79f6b7ac73a0be2
DarkGon12 <44483114+DarkGon12@users.noreply.github.com>
2025-06-07 08:22:12 +0300

    Add files via upload

---
+++ _DATA/DataCode/ConfigsRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using Zenject;
+
+[CreateAssetMenu(fileName = "ConfigRepository", menuName = "Config/Repository")]
+public class ConfigRepository : ScriptableObjectInstaller<ConfigRepository>
+{
+    public List<ConfigBase> configs;
+
+    public override void InstallBindings()
+    {
+        foreach (var config in configs)
+        {
+            Container.Bind(config.GetType()).FromInstance(config).AsSingle().NonLazy();
+        }
+    }
+
+    public T GetConfig<T>() where T : ConfigBase
+    {
+        var config = configs.OfType<T>().FirstOrDefault();
+
+        if (config == null)
+        {
+            Debug.LogWarning($"Config of type {typeof(T).Name} not found.");
+        }
+
+        return config;
+    }
+}
